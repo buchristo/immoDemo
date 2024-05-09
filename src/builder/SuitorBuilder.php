3670682f8ee1db61c7ff4236de8944c69991dcb7
Christopher Bucher <123581943+buchristo@users.noreply.github.com>
2024-05-09 13:25:13 +0200

    create own builder  class which is responsible to build and return a new suitor

---
+++ src/builder/SuitorBuilder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ImmodDemo\Builder;
+
+use ImmoDemo\Model\Suitor;
+use ImmoDemo\Enums\PreferredContact;
+use ImmoDemo\Enums\Request;
+
+class SuitorBuilder {
+    public function build(array $postData): Suitor {
+        $suitor = new Suitor(
+            $postData['title'],
+            $postData['firstName'],
+            $postData['lastName'],
+            $postData['company'],
+            $postData['street'],
+            $postData['streetNumber'],
+            $postData['zipCode'],
+            $postData['city'],
+            $postData['phoneNumber'],
+            $postData['faxNumber'],
+            $postData['mobileNumber'],
+            $postData['email']
+        );
+
+        $postData['contactViaEmail'] && $suitor->addContactPreference(PreferredContact::EMAIL);
+        $postData['contactViaPhone'] && $suitor->addContactPreference(PreferredContact::TELEPHONE);
+        $postData['contactViaMobile'] && $suitor->addContactPreference(PreferredContact::MOBILE);
+        $postData['contactViaFax'] && $suitor->addContactPreference(PreferredContact::FAX);
+        $postData['contactViaLetter'] && $suitor->addContactPreference(PreferredContact::LETTER);
+
+        $postData['requestViewing'] && $suitor->addSuitorRequest(Request::VIEWING);
+        $postData['requestCallback'] && $suitor->addSuitorRequest(Request::CALLBACK);
+        $postData['requestDetails'] && $suitor->addSuitorRequest(Request::DETAIL);
+
+        return $suitor;
+    }
+}
