c5385176af86f137fec2b4db9411b33501c6f87e
Christopher Bucher <123581943+buchristo@users.noreply.github.com>
2024-05-09 14:01:37 +0200

    integrate xmlbuilder in submit.php script and test with postman

--- src/controller/submit.php
+++ src/controller/submit.php
@@ -3,58 +3,15 @@
 require_once __DIR__ . '../../../vendor/autoload.php';
 
 use ImmoDemo\Builder\SuitorBuilder;
+use ImmoDemo\Builder\XmlBuilder;
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $postData = json_decode(file_get_contents("php://input"), true);
 
-
     if ($postData) {
 
         $suitor = SuitorBuilder::build($postData);
-
-//----------------- generate XML --> soll ausgelagert werden in eine eigene klasse
-
-        $xml = new SimpleXMLElement('<AntragformularXML></AntragformularXML>');
-
-        $objekt = $xml->addChild('objekt');
-        $objekt->addChild('object_id', '123');
-        $objekt->addChild('bezeichnung', 'Beispielobjekt');
-
-        $interessent = $xml->addChild('Interessent');
-        $interessent = $xml->addChild('id', $suitor->getId());
-        $interessent = $xml->addChild('anrede', htmlspecialchars($postData['title']));
-        $interessent = $xml->addChild('vorname', htmlspecialchars($postData['firstName']));
-        $interessent = $xml->addChild('nachname', htmlspecialchars($postData['lastName']));
-        $interessent = $xml->addChild('firma', htmlspecialchars($postData['company']));
-        $interessent = $xml->addChild('strasse', htmlspecialchars($postData['street']));
-        $interessent = $xml->addChild('postfach', htmlspecialchars($postData['streetNumber']));
-        $interessent = $xml->addChild('plz', htmlspecialchars($postData['zipCode']));
-        $interessent = $xml->addChild('ort', htmlspecialchars($postData['city']));
-        $interessent = $xml->addChild('tel', htmlspecialchars($postData['phoneNumber']));
-        $interessent = $xml->addChild('fax', htmlspecialchars($postData['faxNumber']));
-        $interessent = $xml->addChild('mobil', htmlspecialchars($postData['mobileNumber']));
-        $interessent = $xml->addChild('email', htmlspecialchars($postData['email']));
-
-        $bevorzugt = $xml->addChild('Bevorzugt');
-        foreach ($suitor->getContactPreferences() as $preference) {
-            $contactNode = $bevorzugt->addChild('Preferred Contact');
-            $contactNode->addAttribute('value', $preference->value);
-        }
-
-        $wunsch = $xml->addChild('Wunsch');
-        foreach ($suitor->getSuitorRequests() as $request) {
-            $requestNode = $wunsch->addChild('Suitor Request');
-            $requestNode->addAttribute('value', $request->value);
-        }
-
-        $dom = new DOMDocument('1.0');
-        $dom->formatOutput = true;
-        $dom->preserveWhiteSpace = false;
-        $domElement = dom_import_simplexml($xml);
-        $domElement = $dom->importNode($domElement, true);
-        $domElement = $dom->appendChild($domElement);
-
-        $dom->save('../../save/suitor.xml');
+        $xmlDocument = XmlBuilder::generateXml($suitor);
 
         header('Content-Type: application/json');
         echo json_encode(['success' => true, 'message' => 'Daten erfolgreich empfangen']);
