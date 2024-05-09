d7dfad0b89d2642912d97533abcc63ad1c0bc3d2
Christopher Bucher <123581943+buchristo@users.noreply.github.com>
2024-05-09 14:00:55 +0200

    create XmlBuilder class which is responsible to create and save an xml file of our client

---
+++ src/builder/XmlBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ImmoDemo\Builder;
+
+use ImmoDemo\Model\Suitor;
+use SimpleXMLElement;
+use DOMDocument;
+
+class XmlBuilder {
+    public static function generateXml(Suitor $suitor): string {
+
+        $xml = new SimpleXMLElement('<AntragformularXML></AntragformularXML>');
+
+        $objekt = $xml->addChild('objekt');
+        $objekt->addChild('object_id', '123');
+        $objekt->addChild('bezeichnung', 'Beispielobjekt');
+
+        $interessent = $xml->addChild('Interessent');
+        $interessent->addChild('id', $suitor->getId());
+        $interessent->addChild('anrede', htmlspecialchars($suitor->getTitle()));
+        $interessent->addChild('vorname', htmlspecialchars($suitor->getFirstName()));
+        $interessent->addChild('nachname', htmlspecialchars($suitor->getLastName()));
+        $interessent->addChild('firma', htmlspecialchars($suitor->getCompany()));
+        $interessent->addChild('strasse', htmlspecialchars($suitor->getStreet()));
+        $interessent->addChild('postfach', htmlspecialchars($suitor->getStreetNumber()));
+        $interessent->addChild('plz', htmlspecialchars($suitor->getZipCode()));
+        $interessent->addChild('ort', htmlspecialchars($suitor->getCity()));
+        $interessent->addChild('tel', htmlspecialchars($suitor->getPhoneNumber()));
+        $interessent->addChild('fax', htmlspecialchars($suitor->getFaxNumber()));
+        $interessent->addChild('mobil', htmlspecialchars($suitor->getMobileNumber()));
+        $interessent->addChild('email', htmlspecialchars($suitor->getEmail()));
+
+        $bevorzugt = $interessent->addChild('Bevorzugt');
+        foreach ($suitor->getContactPreferences() as $preference) {
+            $contactNode = $bevorzugt->addChild('Preferred Contact');
+            $contactNode->addAttribute('value', $preference->value);
+        }
+
+        $wunsch = $interessent->addChild('Wunsch');
+        foreach ($suitor->getSuitorRequests() as $request) {
+            $requestNode = $wunsch->addChild('Suitor Request');
+            $requestNode->addAttribute('value', $request->value);
+        }
+
+        $dom = new DOMDocument('1.0');
+        $dom->formatOutput = true;
+        $dom->preserveWhiteSpace = false;
+        $domElement = dom_import_simplexml($xml);
+        $domElement = $dom->importNode($domElement, true);
+        $domElement = $dom->appendChild($domElement);
+
+        $dom->save('../../save/suitor.xml');
+        return $dom->saveXML();
+    }
+}
